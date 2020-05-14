9d4ae5fe55f5329713d928854a8a8356ff492f4b
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-05-14 14:04:00 +0200

    use strict

---
+++ 07-constants.js
@@ -0,0 +1,12 @@
+"use strict";// Helping us to not make undeclared variables
+//'use strict'; is also working 
+let hello = "hello world"; // ReferenceError: hello is not defined
+
+const MATH_PI = 3.14;
+const FIRST_NAME = "Martina"; // ALL_CAPS for Constants 
+// not possible to change constants! NEVER EVER 
+
+let lastName = "Freundorfer"; // camelCase for Lets 
+lastName = "Mc Donalds"; // Reassignement
+console.log(lastName);
+
