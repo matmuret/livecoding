e46d63393d7ca5b0760bea102c544773cea84239
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-05-14 10:58:39 +0200

    fa

--- 06-changingvars.js
+++ 06-changingvars.js
@@ -1,11 +1,20 @@
 {
-    // IF IN QUESTIONS; ALWAYS CONSOLE LOG 
-    // DEBUGGING = CHECKING 
+    // let change = null, myString = '';
+    let myString = ''; // empty String
+    let change = null; // empty Number
+    let allowedToDrive = false; // starting for a boolean
+    // Read the program? 
 
-    // CHange Variables 
-    let change = 42;
+    // IF IN QUESTIONS; ALWAYS CONSOLE LOG 
+    // DEBUGGING = CHECKING = DO IT AS OFTEN AS YOU WANT
+    // DRY PRINCIPLE = DONT REPEAT YOURSELF
+
+    // CHange Variables "Scope"
+    change = 42; // 42 SAVED 
+    console.log(change); // check the current state 
     // reassign numbers
     change = 28; // 28 saved!!! 
+    console.log(change);
     // add to the number
     change += 8; // 36 saved!!!
     console.log(change); // 36 
@@ -14,7 +23,7 @@
     console.log(change); // 26
 
     // Change String
-    let myString = 'Hello World'; // Hello World
+    myString = 'Hello World'; // Hello World
     console.log(myString); 
     // reassignment of the string
     myString = 'Hello Martina';
@@ -27,5 +36,18 @@
     myString = '';
     console.log(myString);
 
+    // Changing it depending on conditions
+    // Conditional assignment with TERNARY OPERATOR
+    // condition ? true : false;
+    let age = 35;
+    let cash = 10000;
+    //let allowedToDrive = false; // boolean (standard for starters)
 
+    age > 18 ? allowedToDrive = true : allowedToDrive = false;
+    console.log(allowedToDrive);
+
+    let allowedToDriveShorter = age > 18 ? true : false;
+    console.log(allowedToDriveShorter);
+
+    // (marriedStatus === true) // conditions comparison operators === !== >= <= > < 
 }
