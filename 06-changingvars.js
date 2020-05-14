1e581a0ed9f898097f1fb19b775ec79430f0f071
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-05-14 10:01:38 +0200

    changing vars

---
+++ 06-changingvars.js
@@ -0,0 +1,27 @@
+{
+    // CHange Variables 
+    let change = 42;
+    // reassign numbers
+    change = 28; // 28 saved!!! 
+    // add to the number
+    change += 8; // 36 saved!!!
+    console.log(change); // 36 
+    // subtract (minus) to the number
+    change -= 10; // 26 saved !!!
+    console.log(change); // 26
+
+    // Change String
+    let myString = 'Hello World'; // Hello World
+    console.log(myString);
+    // reassignment of the string
+    myString = 'Hello Martina';
+    // add to the String
+    myString += ' How are you today'; // same as myString = myString + 'How are you today'
+    console.log(myString); // ALSO CALLED STRING ACCUMULATION
+    // String CON-CAT-ENATION
+    // empty the string
+    myString = '';
+    console.log(myString);
+
+
+}
